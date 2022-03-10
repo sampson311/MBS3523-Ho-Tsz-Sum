d4110c66ed150a6573eadadfc9b1cb22c399801d
sampson311 <100185648+sampson311@users.noreply.github.com>
2022-03-10 23:33:38 +0800

    Update OpenCV_haar.py

--- OpenCV_haar.py
+++ OpenCV_haar.py
@@ -9,14 +9,16 @@ font = cv2.FONT_HERSHEY_PLAIN
 
 while True:
 
-            unuse, img = cam.read()
+            success, img = cam.read()
             imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             imgGray = cv2.cvtColor(imgGray, cv2.COLOR_GRAY2BGR)
-            faces = face_cascade.detectMultiScale(imgGray, 1.1, 4)
+            faces = face_cascade.detectMultiScale(img, 1.1, 4)
             for (x, y, w, h) in faces:
              cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
-            cv2.putText(img, "MBS3523 Assignment 1c - Q4   Name: Ho Tsz Sum", (125, 40), cv2.FONT_HERSHEY_PLAIN, 2,
-                        (0, 0, 255), 2)
+            cv2.putText(img, "MBS3523 Assignment 1c ", (125, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+            cv2.putText(img, " Q4 Name: Ho Tsz Sum", (125, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+
+
 
             cv2.imshow('Face', img)
 
