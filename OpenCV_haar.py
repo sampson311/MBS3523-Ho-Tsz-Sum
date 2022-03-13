b5091c0de4f76703899f0a6bb3aafc5df6bb7ba6
sampson311 <100185648+sampson311@users.noreply.github.com>
2022-03-13 22:12:01 +0800

    Update OpenCV_haar.py

--- OpenCV_haar.py
+++ OpenCV_haar.py
@@ -1,6 +1,5 @@
 import cv2
 
-
 print(cv2.__version__)
 
 face_cascade = cv2.CascadeClassifier('MBS3523 Resources/haarcascade_frontalface_default.xml')
@@ -10,17 +9,19 @@ font = cv2.FONT_HERSHEY_PLAIN
 while True:
 
             success, img = cam.read()
+            imgCrop = img[100:300, 300:500]
             imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             imgGray = cv2.cvtColor(imgGray, cv2.COLOR_GRAY2BGR)
+            imgGray[100:300, 300:500] = imgCrop
             faces = face_cascade.detectMultiScale(img, 1.1, 4)
             for (x, y, w, h) in faces:
              cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
-            cv2.putText(img, "MBS3523 Assignment 1c ", (125, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-            cv2.putText(img, " Q4 Name: Ho Tsz Sum", (125, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+            cv2.putText(img, "MBS3523 Assignment 1c ", (125, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 125), 3)
+            cv2.putText(img, " Q4 Name: Ho Tsz Sum", (125, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 125), 3)
 
 
 
-            cv2.imshow('Face', img)
+            cv2.imshow('Haar', img)
 
 
             if cv2.waitKey(1) & 0xff == ord('q'):
